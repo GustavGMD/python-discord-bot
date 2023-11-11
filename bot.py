282df1051a6e44e0a8e45fbee173803df0a3d6dc
Gustavo Roman Marangon <gromanmarangon@gmail.com>
2023-11-11 18:48:13 -0300

    add join command

--- bot.py
+++ bot.py
@@ -12,6 +12,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+BOT_DATA_PATH = 'data/bot/'
+ACCOUNTS_DATA_PATH = 'data/accounts/'
+TEMPLATES_DATA_PATH = 'data/templates/'
+
 BOT_CONFIG_FILE = 'bot-config.json'
 INITIALIZED_FIELD = 'initialized'
 GUILD_ID_FIELD = 'guild_id'
@@ -25,6 +29,9 @@ intents.reactions = True
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+def stringToCodeBlock(string :str):
+    return f'```{string}```'
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -37,10 +44,10 @@ async def init(context):
 
     # TODO2: Save the data to a file as JSON  
     try:
-        file = open(BOT_CONFIG_FILE, 'r+')
+        file = open(BOT_DATA_PATH + BOT_CONFIG_FILE, 'r+')
         botConfig = json.load(file)        
     except:
-        file = open(BOT_CONFIG_FILE, 'w')
+        file = open(BOT_DATA_PATH + BOT_CONFIG_FILE, 'w')
         botConfig = dict()
         botConfig[INITIALIZED_FIELD] = False    
 
@@ -53,17 +60,17 @@ async def init(context):
         file.seek(0)
         # Write back to the file
         json.dump(botConfig, file, indent = 6)
-        await context.message.channel.send(
+        await context.message.channel.send(stringToCodeBlock(
             f'Initialization complete at:'
             f'\nGuild: [{context.guild.name}]'
             f'\nChannel: [{context.message.channel.name}]'
-        )   
+        ))   
     else:
-        await context.message.channel.send(
+        await context.message.channel.send(stringToCodeBlock(
             f'I\'m already initialized!'
-            f'\nGuild: [{context.guild.name}]'
-            f'\nChannel: [{context.message.channel.name}]'
-        )       
+            f'\nGuild: [{botConfig[GUILD_ID_FIELD]}]'
+            f'\nChannel: [{botConfig[CHANNEL_ID_FIELD]}]'
+        ))       
 
     # close the file
     file.close()
@@ -78,16 +85,52 @@ async def state(context):
     print("command called: State")
 
 @bot.command(name='join', help='Joins the game! The bot creates an account for the user')
-async def init(context):
-    # Save the user nmame or ID
-    # Save the data to a file, with the starting account data 
-    # Return a feedback message stating that player has joined the game
+async def join(context):
+    # todo1: Get the user name or ID
+    userId = context.author.id
+
+    # todo2:Check if account already exists
+    try:
+        file = open(ACCOUNTS_DATA_PATH + f'account-{userId}.json', 'r', encoding='utf8')
+        # If the file exists, there's alreayd an account so we should just retur
+        # a message to the channel
+        messageString = f'You already have an account {context.author.display_name}'
+        await context.message.channel.send(stringToCodeBlock(messageString))
+        file.close()
+    except:
+        # the account file doesn't exist
+        messageString = f'You don\'t have an account yet {context.author.display_name}'
+        await context.message.channel.send(stringToCodeBlock(messageString))
+
+        # Load template account and fill with the new player's data
+        # Save file
+        file = open(ACCOUNTS_DATA_PATH + f'account-{userId}.json', 'w', encoding='utf8')
+        templateFile = open(TEMPLATES_DATA_PATH + 'account-template.json', 'r', encoding='utf8')
+        account = json.load(templateFile)
+        account['userid'] = userId
+        file.seek(0)
+        json.dump(account, file, indent = 6)
+
+        messageString = f'New account created! {context.author.display_name}'
+        for emoji in account['emojis']:
+            # messageString += f'U+{ord(emoji):X}'
+            messageString += f'{emoji} '        
+        await context.message.channel.send(stringToCodeBlock(messageString))
+
+        # Close the open files
+        file.close()
+        templateFile.close()
+
+        
+    # todo3: Save the data to a file, with the starting account data 
+    # todo4: Return a feedback message stating that player has joined the game
     print("command called: Join")
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
     channel = bot.get_channel(payload.channel_id)
-    await channel.send(f'You reacted with {payload.emoji}')
+    await channel.send(stringToCodeBlock(f'You reacted with {payload.emoji}'))
     # Check if the user is allowed to use this emoji
     # if it is, add it to the current activity count
     # else remove the reaction
